6548a5b5673ad0540f663ad8d6870c03ac3a3b9e
SoupaNut <156037519+SoupaNut@users.noreply.github.com>
2025-03-11 09:48:51 -0500

    Update 88.merge-sorted-array.py

--- 88.merge-sorted-array.py
+++ 88.merge-sorted-array.py
@@ -11,5 +11,19 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
+        midx = m - 1
+        nidx = n - 1
+        right = m + n - 1
+
+        while nidx >= 0:
+            if midx >= 0 and nums1[midx] > nums2[nidx]:
+                nums1[right] = nums1[midx]
+                midx -= 1
+            else:
+                nums1[right] = nums2[nidx]
+                nidx -= 1
+
+            right -= 1
+        
 # @lc code=end
 
